ada276b5222f939eefe3270b5c6bd6da282af4f6
Pango Gillespi <commander.commando@gmail.com>
2022-11-29 13:57:24 -0500

    fix some scripts or whatever (#6)
    Co-authored-by: Pango Gillespi <pango@tox.one>

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 COMPOSER_PATH="/usr/local/bin/composer"
+PHP_USER="www-data"
+PHP_GROUP="www-data"
 
-while getopts "u:g:" OPT; do
+while getopts "c:u:g:h" OPT; do
     case $OPT in
         u)
             PHP_USER="${OPTARG}"
@@ -13,8 +15,55 @@ while getopts "u:g:" OPT; do
         c)
             COMPOSER_PATH="${OPTARG}"
             ;;
+        h)
+            show_help
+            exit 0
+            ;;
+        *)
+            echo "unknown option $OPT"
+            exit 1
     esac
 done
+
+show_help() {
+    cat << EOF
+    Setup the ridiculous PHP App Framework Which Exists for No Reason
+
+    Usage:
+    $0 [-u <php user> | -g <php group> | -c <composer path> | -h]
+
+    Options:
+        -u <php user>       the system user that runs PHP (for r/w access to this folder)
+        -g <php group>      the group of the user that runs PHP
+        -c <composer path>  absolute path to composer
+        -h                  show this guide and exit
+
+    This will install Composer dependencies (including Composer itself if
+    it can't be found). It assumes your php user and group are both
+    "www-data", so use the flags if that is not true in your case.
+
+    Assuming it works, you should be able to log in to the app using the
+    username "admin" and password "password".
+EOF
+}
+
+setup_composer() {
+    EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+    if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+    then
+        >&2 echo 'ERROR: Invalid installer checksum'
+        rm composer-setup.php
+        echo 1
+    fi
+
+    php composer-setup.php --quiet --install-dir=/usr/local/bin --filename=composer
+    RESULT=$?
+    rm composer-setup.php
+    echo $RESULT
+}
 
 if [ -f utils/settings.json ]; then
     echo "it looks like you're already set up. if you want to rerun this, delete utils/settings.json and try again."
@@ -40,21 +89,20 @@ cp -o utils/settings.json.setup utils/settings.json
 cp -o utils/composer_base.json composer/composer.json
 
 if [[ ! -f $COMPOSER_PATH ]]; then
+    cd ./composer || exit 1
     echo "installing composer..."
-    php composer-setup.php --install-dir=/usr/local/bin --filename=composer > /dev/null 2>&1
-    if [ $? -gt 0 ]; then
+    COMPINST=$( composer_setup )
+    if [ "$COMPINST" -gt 0 ]; then
         echo "composer didn't install, i give up."
         exit 1
     else
         echo "installing dependencies ..."
         /usr/local/bin/composer update >> /var/log/tox_composer_update.log 2>&1
     fi
+    cd ..
 fi
 
-if [ -z $PHP_USER ]; then read -r -p "PHP User: " PHP_USER; fi
-if [ -z $PHP_GROUP]; then read -r -p "PHP Group: " PHP_GROUP; fi
-
-chown -R ${PHP_USER}:${PHP_GROUP} ./
+chown -R "${PHP_USER}:${PHP_GROUP}" ./
 chmod -R +rwX ./
 
 cat <<EOF > run_update.sh
